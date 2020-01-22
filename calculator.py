3add6dcb623e111e82d0f2c34c7a63e3e8104b5b
athanikos <athanikos@gmail.com>
2020-01-22 12:27:21 +0200

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,14 @@
+
+def compute_average_goals(flat_matches, team_id):
+    average_goals = 0
+    count = 0
+    for ft in flat_matches:
+        if ft.localteam_id == team_id:
+            average_goals += ft.stats_data_0_goals
+            count = count + 1
+        elif ft.visitorteam_id == team_id:
+            average_goals += ft.stats_data_1_goals
+            count = count + 1
+    return average_goals / float(count)
+
+
