0077e03759cb96ba971233e5531ce801404665b1
nikos <athanikos@gmail.com>
2020-01-25 01:21:14 +0200

    uses blueprint for flask app, tests flask client

--- FootBotApi/server.py
+++ FootBotApi/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Blueprint
 import logging
 
 from FootBotApi.calculator import build_stats, OutputTeamStats
@@ -6,25 +6,36 @@ from FootBotApi.models import flatmatches
 from mongoengine import connect
 from mongoengine.queryset.visitor import Q
 
-app = Flask(__name__)
+
+# app = Flask(__name__)
+bp = Blueprint('myapp', __name__)
+
 logging.basicConfig(level=logging.DEBUG)
 server_name = "localhost"
 port = 27017
 database_name = "book"
 
 
-@app.route('/')
+def create_app(config_filename=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.register_blueprint(bp)
+    config_filename = "/home/nikos/PycharmProjects/FootBotApi/flask.cfg"
+    app.config.from_pyfile(config_filename)
+    return app
+
+
+@bp.route('/')
 def hello_world():
     return 'Hello from FootBot Api !'
 
 
-@app.route("/api/v1/matches/<int:league_id>/<int:team_id>/<before_date>/<time_status>", methods=['GET'])
+@bp.route("/api/v1/matches/<int:league_id>/<int:team_id>/<before_date>/<time_status>", methods=['GET'])
 def get_flat_matches(league_id, team_id, before_date, time_status):
     items = fetch_flat_matches(before_date, league_id, team_id, time_status)
     return jsonify(items.to_json())
 
 
-@app.route("/api/v1/stats/<int:league_id>/<int:team_id>/<before_date>/<time_status>", methods=['GET'])
+@bp.route("/api/v1/stats/<int:league_id>/<int:team_id>/<before_date>/<time_status>", methods=['GET'])
 def get_stats(league_id, team_id, before_date, time_status):
     items = fetch_flat_matches(before_date, league_id, team_id, time_status)
     output = OutputTeamStats()
@@ -34,30 +45,22 @@ def get_stats(league_id, team_id, before_date, time_status):
 
 def fetch_flat_matches(before_date, league_id, team_id, time_status):
     connect(database_name, host=server_name, port=port)
-    items = flatmatches.objects((Q(localteam_id=team_id) | Q(visitorteam_id=team_id)) & Q(time_status=time_status)
-                          & Q(league_id=league_id) & Q(time_starting_at_date__lte=before_date)).order_by('time_starting_at_date-')[:10]
-    app.logger.info(league_id)
-    app.logger.info(team_id)
-    app.logger.info(before_date)
-    app.logger.info(time_status)
-
+    items = flatmatches.objects((Q(localteam_id=team_id) | Q(visitorteam_id=team_id))
+                                & (Q(time_status=time_status) )
+                                & Q(league_id=league_id) & Q(time_starting_at_date__lte=before_date)).order_by(
+        'time_starting_at_date-')[:10]
     return items
 
 
-@app.before_request
+@bp.before_request
 def before():
-    app.logger.info(request.headers)
-    app.logger.info(request.data)
     pass
 
 
-@app.after_request
+@bp.after_request
 def after(response):
-    app.logger.info(response.status)
-    app.logger.info(response.headers)
-    app.logger.info(response.get_data())
     return response
 
 
 if __name__ == '__main__':
-    app.run()
+    bp.run()
---
+++ FootBotApi/test/server_test.py
@@ -0,0 +1,34 @@
+from types import MethodType
+
+import mock
+import pytest
+from flask import jsonify
+from FootBotApi.models import flatmatches
+from FootBotApi.server import get_flat_matches, create_app
+import json
+
+@pytest.fixture(scope='module')
+def test_client():
+    flask_app = create_app('flask.cfg')
+    testing_client = flask_app.test_client()
+    ctx = flask_app.app_context()
+    ctx.push()
+    yield testing_client  # this is where the testing happens!
+    ctx.pop()
+
+
+@pytest.fixture
+def mock_get_value():
+    with mock.patch(
+            "FootBotApi.server.fetch_flat_matches",
+            autospec=True,
+    ) as _mock:
+        ft = flatmatches()
+        ft.league_id = -1
+        yield ft
+
+
+def test_get_flat_matches(mock_get_value, test_client):
+    response = test_client.get('/api/v1/stats/72/629/2020-01-20/FT')
+    assert response.status_code == 200
+   # assert json.loads(response.get_data())['league_id'] == 72
