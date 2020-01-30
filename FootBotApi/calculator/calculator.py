9e410f6cb16bddb7a9677ff5f3257e3a09f8b28d
nikos <athanikos@gmail.com>
2020-01-30 22:03:36 +0200

    CalculatedVariable

--- FootBotApi/calculator/calculator.py
+++ FootBotApi/calculator/calculator.py
@@ -1,10 +1,17 @@
 import json
 
+from FootBotApi.calculator.CalculatedVariable import CalculatedVariable, Calculation_Method
+
 CNT = "cnt"
 SUM = "sum"
-input_output_pairs = {"stats_data_0_goals": 'out', "stats_data_1_goals": 'out2'}
+
 
 variables = []
+first = CalculatedVariable(1, 1, 1, 'stats_data_0_goals', 'out', Calculation_Method.AVG)
+second = CalculatedVariable(1, 1, 1, 'stats_data_1_goals', 'out2', Calculation_Method.AVG)
+variables.append(first)
+variables.append(second)
+
 
 class OutputTeamStats(object):
     def toJSON(self):
@@ -12,70 +19,11 @@ class OutputTeamStats(object):
 
 
 def build_stats(flat_matches, team_id, league_id, before_date,object_to_set):
-    compute_average(flat_matches,team_id,input_output_pairs, object_to_set)
+    compute_average(flat_matches, team_id, variables, object_to_set)
     setattr(object_to_set, "team_id", team_id)
     setattr(object_to_set, "league_id", league_id)
     setattr(object_to_set, "before_date", before_date)
 
 
 def compute_average(flat_matches, team_id, variable_pairs, object_to_set):
-    for variable in variable_pairs:
-        key = variable.input_variable_name
-        init_value(variable_pairs, key, object_to_set, CNT)
-        init_value(variable_pairs, key, object_to_set, SUM)
-        for ft in flat_matches:
-            if not hasattr(ft, key):
-                raise NameError(key + " does not exist ")
-            if ft.localteam_id == team_id:
-                increment_sum_value(variable_pairs, key, object_to_set, getattr(ft, key))
-                set_next_count_value(variable_pairs, key, object_to_set)
-            elif ft.visitorteam_id == team_id:
-                increment_sum_value(variable_pairs, key, ft, getattr(ft, key))
-                set_next_count_value(variable_pairs, key, object_to_set)
-    for variable in variable_pairs:
-        key = variable.input_variable_name
-        if get_count_value(variable_pairs, key, object_to_set) == 0:
-            setattr(object_to_set, variable_pairs[key], 0)
-        else:
-            setattr(object_to_set, variable_pairs[key], get_sum_value(variable_pairs, key, object_to_set)
-                    / float(get_count_value(variable_pairs, key, object_to_set)))
-    return object_to_set
-
-
-def get_count_value(name_pairs, key, object_to_get_from):
-    if not hasattr(object_to_get_from, name_pairs[key] + CNT):
-        setattr(object_to_get_from, name_pairs[key] + CNT, 0)
-    return getattr(object_to_get_from, name_pairs[key] + CNT)
-
-
-def get_value(name_pairs, key, object_to_get_from):
-    if not hasattr(object_to_get_from,  key):
-        setattr(object_to_get_from, key, 0)
-    return getattr(object_to_get_from, key)
-
-
-def get_sum_value(name_pairs, key, object_to_get_from):
-    if not hasattr(object_to_get_from, name_pairs[key] + SUM):
-        setattr(object_to_get_from, name_pairs[key] + SUM, 0)
-    return getattr(object_to_get_from, name_pairs[key] + SUM)
-
-
-def set_next_count_value(name_pairs, key, object_to_get_from):
-    if not hasattr(object_to_get_from, name_pairs[key] + CNT):
-        setattr(object_to_get_from, name_pairs[key] + CNT, 0)
-    setattr(object_to_get_from, name_pairs[key] + CNT, get_count_value(name_pairs, key, object_to_get_from) + 1)
-
-
-def init_value(name_pairs, key, object_to_get_from, postfix):
-    setattr(object_to_get_from, name_pairs[key] + postfix, 0)
-
-
-def increment_sum_value(name_pairs, key, object_to_get_from, value):
-    if value is None:
-        value = 0
-
-    if not hasattr(object_to_get_from, name_pairs[key] + SUM):
-        setattr(object_to_get_from, name_pairs[key] + SUM, value)
-    else:
-        setattr(object_to_get_from, name_pairs[key] + SUM,
-                get_sum_value(name_pairs, key, object_to_get_from) + value)
+  pass
--- FootBotApi/test/calculator_test.py
+++ FootBotApi/test/calculator_test.py
@@ -1,49 +1,8 @@
 import pytest
 
-from FootBotApi.calculator.VariablePair import VariablePair, Calculation_Method
+from FootBotApi.calculator.CalculatedVariable import CalculatedVariable, Calculation_Method
 from FootBotApi.calculator.calculator import OutputTeamStats, compute_average, init_value, build_stats, SUM, get_value
 from FootBotApi.test.helpers import get_match
-
-
-def test_compute_average_variable_not_in_place():
-    matches = []
-    match1 = get_match(1, 1, 2, 1, 0)
-    match2 = get_match(1, 1, 2, 1, 1)
-    matches.append(match1)
-    in_out_pairs = {"stats_data_0_goals": 'out', "dummy": 'out2'}
-
-    object_to_set = OutputTeamStats()
-    with pytest.raises(NameError):
-        compute_average(matches, 1, in_out_pairs, object_to_set)
-
-
-def test_compute_average():
-    matches = []
-    match1 = get_match(1, 1, 2, 1, 0)
-    match2 = get_match(1, 1, 2, 1, 1)
-    matches.append(match1)
-    matches.append(match2)
-    object_to_set = OutputTeamStats()
-    in_out_pairs = {"stats_data_0_goals": 'out', "stats_data_1_goals": 'out2'}
-    compute_average(matches, 1, in_out_pairs, object_to_set)
-    assert object_to_set.out == 1
-    assert object_to_set.out2 == .5
-
-
-def increment_average_value(in_out_pairs, param, object_to_set, param1):
-    pass
-
-
-def test_increment_average_value():
-    matches = []
-    match1 = get_match(1, 1, 2, 1, 0)
-    matches.append(match1)
-    object_to_set = OutputTeamStats()
-    in_out_pairs = {"stats_data_0_goals": 'out'}
-    init_value(in_out_pairs, "stats_data_0_goals", object_to_set, SUM)
-    increment_average_value(in_out_pairs, "stats_data_0_goals", object_to_set, None)
-    assert getattr(object_to_set, "out" + SUM) == 0
-
 
 def test_build_stats():
     matches = []
@@ -51,7 +10,7 @@ def test_build_stats():
     matches.append(match1)
     object_to_set = OutputTeamStats()
     variable_pairs =[]
-    variable = VariablePair(1, 1, 1,  'stats_data_0_goals', 'out', Calculation_Method.AVG)
+    variable = CalculatedVariable(1, 1, 1, 'stats_data_0_goals', 'out', Calculation_Method.AVG)
     variable_pairs.append(variable)
     build_stats(matches, 1, 1, "1/1/2010", object_to_set)
     assert getattr(object_to_set, "team_id") == 1
