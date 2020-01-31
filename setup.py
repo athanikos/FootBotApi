5b66300ff3b5662d4bad5d6fff174b9dbd11f545
nikos <athanikos@gmail.com>
2020-01-31 21:59:40 +0200

    adds LICENSE and setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+import FootBotApi
+from distutils.core import setup
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='FootBotApi',
+    version="0.0.1",
+    license='Apache Software License',
+    author='Nikos Athanasakis',
+    packages=setuptools.find_packages(),
+    author_email='athanikos@jgmail.com',
+    description='A set of rest api methods used for calculating statistics and applying models for football data ',
+    tests_require=['pytest'],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
