affd3ae220956e5de443815fda76e26a0187b66c
nikos <athanikos@gmail.com>
2020-02-01 14:30:15 +0200

    uses current_app for config.app

--- FootBotApi/server.py
+++ FootBotApi/server.py
@@ -5,18 +5,16 @@ from FootBotApi.models import flatmatches
 from mongoengine import connect
 from mongoengine.queryset.visitor import Q
 from FootBotApi.config import BaseConfig, configure_app
-
+from flask import current_app as app
 
 bp = Blueprint('myapp', __name__)
 
 
 def create_app():
-    app = Flask(__name__, instance_relative_config=True)
-    configure_app(app)
-    app.register_blueprint(bp)
-    return app
-
-
+    the_app = Flask(__name__, instance_relative_config=True)
+    configure_app(the_app)
+    the_app.register_blueprint(bp)
+    return the_app
 
 
 @bp.route("/api/v1/matches/<int:league_id>/<int:team_id>/<before_date>/<time_status>", methods=['GET'])
@@ -34,9 +32,7 @@ def get_stats(league_id, team_id, before_date, time_status):
 
 
 def fetch_flat_matches(before_date, league_id, team_id, time_status):
-    #connect(app.config['DATABASE'], host=app.config['SERVERNAME'], port=app.config['PORT'])
-    connect(BaseConfig.DATABASE, host=BaseConfig.SERVERNAME, port=BaseConfig.PORT)
-
+    connect(app.config['DATABASE'], host=app.config['SERVERNAME'], port=app.config['PORT'])
     return flatmatches.objects((Q(localteam_id=team_id) | Q(visitorteam_id=team_id))
                                & (Q(time_status=time_status))
                                & Q(league_id=league_id) & Q(time_starting_at_date__lte=before_date)).order_by(
