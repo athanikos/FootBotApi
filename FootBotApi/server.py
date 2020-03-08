d48746d29fe26e410c1f67b6ba5ed22ec0334b15
nikos <athanikos@gmail.com>
2020-03-08 17:07:57 +0200

    circle ci

--- FootBotApi/server.py
+++ FootBotApi/server.py
@@ -2,10 +2,10 @@ from flask import Flask, jsonify, Blueprint
 from flask import current_app as app
 from mongoengine import connect
 from mongoengine.queryset.visitor import Q
-from FootBotApi.calculator.ComputedFromEventsFields import ComputedFromEventsFields, minutes
-from FootBotApi.calculator.calculator import build_computed_stats, build_historical_stats, OutputTeamStats
-from FootBotApi.config import configure_app
-from FootBotApi.models import flatmatches, matches
+from ..FootBotApi.calculator.ComputedFromEventsFields import ComputedFromEventsFields, minutes
+from ..FootBotApi.calculator.calculator import build_computed_stats, build_historical_stats, OutputTeamStats
+from ..FootBotApi.config import configure_app
+from ..FootBotApi.models import flatmatches, matches
 
 
 bp = Blueprint('myapp', __name__)
